ec48b27a30a5537fa7893ef96bf56d5a86a9a657
btypoon <super00bird@gmail.com>
2025-07-08 01:31:07 +0800

    update

--- script/script.js
+++ script/script.js
@@ -4,58 +4,58 @@
   // Create a new XMLHttpRequest object
   var xhr = new XMLHttpRequest();
   // Make a GET request to the file
-  xhr.open('GET', 'template.html', true);
+  xhr.open("GET", "template.html", true);
   // Set the response type to text. Default type: text
-  xhr.responseType = 'document';
+  xhr.responseType = "document";
   // When the request is complete
-  xhr.onload = function() {
+  xhr.onload = function () {
     if (this.status === 200) {
       var templateDocument = xhr.response;
-      addHeader(templateDocument);
+      addNavBar(templateDocument); //prepend firstly
+      addHeader(templateDocument); //prepend secondly
       addFooter(templateDocument);
-      addNavBar(templateDocument);
       addLinkCSS(templateDocument);
       addLinkScript(templateDocument, 1);
       setTimeout(addLinkScript, 500, templateDocument, 2);
-    };
+    }
   };
   xhr.send();
 })();
 
 function addHeader(templateDocument) {
-  var template = templateDocument.querySelector('#navHeaderTemplate');
+  var template = templateDocument.querySelector("#navHeaderTemplate");
   var clone = template.content.cloneNode(true);
-  document.querySelector('main').before(clone);
-  document.head.prepend(templateDocument.querySelector('#navHeaderStyle'));
-
-};
-
-function addFooter(templateDocument) {
-  var template = templateDocument.querySelector('#navFooterTemplate');
-  var clone = template.content.cloneNode(true);
-  document.querySelector('main').after(clone);
-  document.head.prepend(templateDocument.querySelector('#navFooterStyle'));
-};
+  document.body.prepend(clone);
+  document.head.prepend(templateDocument.querySelector("#navHeaderStyle"));
+}
 
 function addNavBar(templateDocument) {
-  var template = templateDocument.querySelector('#navBarTemplate');
+  var template = templateDocument.querySelector("#navBarTemplate");
   var clone = template.content.cloneNode(true);
-  document.querySelector('main').before(clone);
-  document.head.prepend(templateDocument.querySelector('#navBarStyle'));
-};
+  document.body.prepend(clone);
+  document.head.prepend(templateDocument.querySelector("#navBarStyle"));
+}
 
-function addLinkCSS(templateDocument) {
-  var template = templateDocument.querySelector('#linkCSS');
-  var clone = template.content.cloneNode(true);
-  document.head.prepend(clone);
-};
-
-function addLinkScript(templateDocument, index) {
-  var template = templateDocument.querySelector('#linkScript' + index.toString());
+function addFooter(templateDocument) {
+  var template = templateDocument.querySelector("#navFooterTemplate");
   var clone = template.content.cloneNode(true);
   document.body.append(clone);
-};
+  document.head.prepend(templateDocument.querySelector("#navFooterStyle"));
+}
 
+function addLinkCSS(templateDocument) {
+  var template = templateDocument.querySelector("#linkCSS");
+  var clone = template.content.cloneNode(true);
+  document.head.prepend(clone);
+}
+
+function addLinkScript(templateDocument, index) {
+  var template = templateDocument.querySelector(
+    "#linkScript" + index.toString()
+  );
+  var clone = template.content.cloneNode(true);
+  document.body.append(clone);
+}
 
 function getButton() {
   mybuttonDiv = document.querySelector(".bottomRightButtons");
@@ -63,14 +63,14 @@ function getButton() {
   mybuttonDiv.style.display = "none";
 
   if (mybutton == null) {
-    console.log('mybutton does not exist in the DOM');
+    console.log("mybutton does not exist in the DOM");
     console.log(mybutton);
   } else {
-    console.log('mybutton exist in the DOM');
+    console.log("mybutton exist in the DOM");
     console.log(mybutton);
   }
 
-  window.onscroll = function() {
+  window.onscroll = function () {
     // When the user scrolls down 20px from the top of the document, show the button
     if (
       document.body.scrollTop > 115 ||
@@ -82,14 +82,12 @@ function getButton() {
     }
   };
 
-
   // When the user clicks on the button, scroll to the top of the document
-  mybutton.addEventListener("click", function() {
+  mybutton.addEventListener("click", function () {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
   });
-
-};
-window.addEventListener("load", function() {
+}
+window.addEventListener("load", function () {
   setTimeout(getButton, 500);
 });
